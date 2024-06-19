f0b9daf44f26c0d56c7e4732c740eb28898442bd
GeneralB5 <ianmarcobonfiglio@gmail.com>
2024-06-19 19:44:07 -0300

    adding endpoints and swagger

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(app.Environment.IsDevelopment()){
 
+    app.UseSwagger();
+    app.UseSwaggerUI(c=>{
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });    
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/products", () => "data");
+app.MapGet("/products/{id}", (int id) =>  "id : "+id);
+app.MapPost("/product/create",( ) => {});
+app.MapPut("/product/",()=>{});
+app.MapDelete("/product/{id}",(int id)=>{});
 app.Run();
--- PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d17b14bfa9b3637a9a5144c5048eb3662e958a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
